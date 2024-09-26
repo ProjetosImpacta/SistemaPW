5c95ad7b257da975ffe91c19ee48b772c9d3e103
Gustavo Melo <gustavo.melo.fn@gmail.com>
2024-09-26 08:46:59 -0300

    fix cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,40 +1,36 @@
-//JS principal do projeto MVC
 const url = 'https://go-wash-api.onrender.com/api/user'
+
 async function cadastrousuario(){
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let cpf_cnpj = document.getElementById('cpf_cnpj').value
-    let password = document.getElementById('password').value
-    let birthday = document.getElementById('birthday').value
-    let terms = document.getElementById('terms').checked
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let cpf_cnpj = document.getElementById('cpf_cnpj').value;
+    let birthday = document.getElementById('birthday').value;
+    let terms = document.getElementById('terms').checked;
+
     let api = await fetch(url,{
         method:"POST",
         body:JSON.stringify(
             {
                 "name":name,
-                "email": email,
+                "email":email,
                 "user_type_id":1,
                 "password": password,
                 "cpf_cnpj": cpf_cnpj,
                 "terms": 1,
-                "birthday": birthday    
+                "birthday":"2000-10-12"    
             }
         ),
         headers:{
             'Content-Type': 'application/json'
-        }  
-
+        }
     });
 
-    if (api.ok){
+    if(api.ok){
         let resposta = await api.json();
         console.log(resposta);
         return;
     }
     let respostaErro = await api.json();
-    alert(respostaErro.data.errors.cpf_cnpj[0])
-
+    alert(respostaErro.data.errors.cpf_cnpj);
 }
-
-
-
