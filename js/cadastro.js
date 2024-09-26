b1e6be16df4c46964d2b5cec49d083498557a9e0
Gustavo Melo <gustavo.melo.fn@gmail.com>
2024-09-26 09:33:05 -0300

    verificação dos termos

--- js/cadastro.js
+++ js/cadastro.js
@@ -6,34 +6,39 @@ async function cadastrousuario(){
     let password = document.getElementById('password').value;
     let cpf_cnpj = document.getElementById('cpf_cnpj').value;
     let birthday = document.getElementById('birthday').value;
-    let terms = document.getElementById('terms').checked;
-
-    let api = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify(
-            {
-                "name":name,
-                "email":email,
-                "user_type_id":1,
-                "password": password,
-                "cpf_cnpj": cpf_cnpj,
-                "terms": terms,
-                "birthday":"2000-10-12"    
+    let terms = document.getElementById('terms');
+    
+    if (terms.checked == true){
+        let api = await fetch(url,{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    "name":name,
+                    "email":email,
+                    "user_type_id":1,
+                    "password": password,
+                    "cpf_cnpj": cpf_cnpj,
+                    "terms": 1,
+                    "birthday":"2000-10-12"    
+                }
+            ),
+            headers:{
+                'Content-Type': 'application/json'
             }
-        ),
-        headers:{
-            'Content-Type': 'application/json'
+        });
+    
+        if(api.ok){
+            let resposta = await api.json();
+            console.log(resposta);
+            alert('Cadastrado com sucesso!');
+            window.location.href = "../index.html"
+            return;
         }
-    });
-
-    if(api.ok){
-        let resposta = await api.json();
-        console.log(resposta);
-        alert('Cadastrado com sucesso!');
-        window.location.href = "../index.html"
-        return;
+        let respostaErro = await api.json();
+        alert(respostaErro.data.errors.cpf_cnpj);
+        alert(respostaErro.data.errors.email);
+    }else{
+        alert('Aceite os termos de uso!')
     }
-    let respostaErro = await api.json();
-    alert(respostaErro.data.errors.cpf_cnpj);
-    alert(respostaErro.data.errors.email);
 }
+    
